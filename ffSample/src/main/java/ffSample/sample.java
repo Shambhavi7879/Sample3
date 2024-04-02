6b5cc74051ffc371b73dbcc4834c0e6ad86b9d37
Shambhavi <Shambhavi@TYSS-Shambhavi>
2024-04-02 18:57:08 +0530

    1st push

---
+++ ffSample/src/main/java/ffSample/sample.java
@@ -0,0 +1,23 @@
+package ffSample;
+
+public class sample {
+	
+	
+		public static void main(String[] args) {
+			String s1="abc";
+			String s2="abc";
+			boolean result;
+			
+			if(s1.equals(s2))
+			{
+				System.out.println("Two Strings are equal");
+			}
+			else
+			{
+				System.out.println("Two Strings are not equal");
+			}
+
+		}
+
+	}
+
